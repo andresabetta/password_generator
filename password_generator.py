0e12596e4f2e33223c378741a42f6095d7483a87
andre <aandrev89@gmail.com>
2025-03-18 13:00:56 -0300

    Added my password generator

---
+++ password_generator.py
@@ -0,0 +1,42 @@
+import string
+import random
+
+def main():
+    print("=== Password Generator ===")
+    
+    while True:
+        try:
+            digitos = int(input("Enter password length (min 8): "))
+            if digitos >=8:
+                break
+            else:
+                print("Minimum length is 8.")
+        except ValueError:
+            print("Enter a valid number.")
+
+    
+
+    print(password_generator(digitos))
+
+def password_generator(lenght=12):
+    letter = string.ascii_letters
+    number = string.digits
+    special_char = "!@#$%^&*()_+-="
+    allchars = letter+number+special_char
+
+    password = [
+        random.choice(letter),
+        random.choice(number),
+        random.choice(special_char)
+    ]
+
+    for _ in range(lenght-3):
+        password.append(random.choice(allchars))
+    
+    random.shuffle(password)
+
+    return "".join(password)
+
+
+if __name__ == "__main__":
+    main()
